0cd0c9ef06c251d3c88ed66baedf2ed0df0ed90a
Giovanni Cascio <giova-@live.de>
2024-04-30 12:17:08 +0200

    feat: add e2e tests with playwright

---
+++ tests/home.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('has heading', async ({ page }) => {
+  await page.goto('/');
+
+  const heading = page.locator('h1');
+
+  await expect(heading).toBeVisible();
+});
